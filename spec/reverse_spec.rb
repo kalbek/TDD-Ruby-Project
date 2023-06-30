c9bc29a35fb6f98c63efd55b5e311cb8d472485f
kalbek <access2kaleb@gmail.com>
2023-06-30 11:18:47 +0300

    Prepare test for reverse method.

---
+++ spec/reverse_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative '../solver'
+
+RSpec.describe Solver do
+  describe '#reverse' do
+    it 'returns the reversed word' do
+      solver = Solver.new
+      word = 'hello'
+      reversed_word = solver.reverse(word)
+      expect(reversed_word).to eq('olleh')
+    end
+  end
+end
